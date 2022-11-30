11149b774599482e0a541f37bbbffe53419c5fe6
DoooReyn <jl88744653@gmail.com>
2022-11-30 15:43:22 +0800

    EventScene

---
+++ client/assets/scripts/scene/event/event_scene.ts
@@ -0,0 +1,40 @@
+import { _decorator, Component, Node, EventTouch } from "cc";
+import { MyEventTouch } from "./my_event_touch";
+
+const { ccclass, property } = _decorator;
+
+/**
+ * Url      : db://assets/scripts/scene/event/event_scene.ts
+ * Author   : reyn
+ * Date     : Wed Nov 30 2022 13:45:55 GMT+0800 (中国标准时间)
+ * Class    : EventScene
+ * Desc     :
+ */
+@ccclass("EventScene")
+export class EventScene extends Component {
+    @property(MyEventTouch)
+    red: MyEventTouch = null;
+
+    @property(MyEventTouch)
+    green: MyEventTouch = null;
+
+    /************************************************************
+     * 基础事件
+     ************************************************************/
+
+    onBtnClicked(e: EventTouch, type: string) {
+        switch (type) {
+            case "0":
+            case "1":
+            case "2":
+                const et = parseInt(type);
+                this.red.setTouchEventType(et);
+                this.green.setTouchEventType(et);
+                break;
+        }
+    }
+
+    // update(dt: number) {}
+
+    // lateUpdate(dt: number) {}
+}
---
+++ client/assets/scripts/scene/event/my_event_touch.ts
@@ -0,0 +1,114 @@
+import {
+    _decorator,
+    Component,
+    NodeEventType,
+    Event,
+    UITransform,
+    EventTouch,
+    Sprite,
+    Color,
+} from "cc";
+import { Singletons } from "../../base/singletons";
+const { ccclass } = _decorator;
+
+class MyEvent extends Event {
+    // 自定义的属性
+    public detail: any = null;
+
+    constructor(name: string, bubbles?: boolean, detail?: any) {
+        super(name, bubbles);
+        this.detail = detail;
+    }
+}
+
+/**
+ * Url      : db://assets/scripts/scene/event/event_touch.ts
+ * Author   : reyn
+ * Date     : Wed Nov 30 2022 14:23:35 GMT+0800 (中国标准时间)
+ * Class    : EventTouch
+ * Desc     :
+ */
+
+enum E_TouchType {
+    SwallowForbidden = 0,
+    SwallowAllowedButRectLimited,
+    SwallowAllowedWithoutRectLimited,
+}
+
+@ccclass("MyEventTouch")
+export class MyEventTouch extends Component {
+    private _touchType: E_TouchType = E_TouchType.SwallowForbidden;
+    private _color: Color = null;
+
+    /************************************************************
+     * 基础事件
+     ************************************************************/
+
+    onLoad() {
+        this._color = this.node.getComponent(Sprite).color.clone();
+    }
+
+    onDestroy() {}
+
+    onEnable() {
+        this.node.on(NodeEventType.TOUCH_START, this._onTouchedStart, this);
+        this.node.on(NodeEventType.TOUCH_END, this._onTouched, this);
+        this.node.on(NodeEventType.TOUCH_CANCEL, this._onTouched, this);
+        this.node.on("clicked", this._onClicked, this);
+    }
+
+    onDisable() {
+        this.node.off(NodeEventType.TOUCH_END, this._onTouched, this);
+        this.node.off("clicked", this._onClicked, this);
+    }
+
+    public setTouchEventType(t: E_TouchType) {
+        this._touchType = t;
+    }
+
+    private _onTouchedStart(e: EventTouch) {
+        const transform = this.node.getComponent(UITransform);
+        switch (this._touchType) {
+            case E_TouchType.SwallowForbidden:
+                // 不允许穿透
+                if (e.eventPhase === Event.CAPTURING_PHASE) {
+                    transform.hitTest(e.getLocation()) && this._setClicked();
+                } else {
+                    e.propagationStopped = true;
+                    this._setClicked();
+                }
+                break;
+            case E_TouchType.SwallowAllowedButRectLimited:
+                // 允许穿透，且限定落点在节点内部
+                transform.hitTest(e.getLocation()) && this._setClicked();
+                break;
+            case E_TouchType.SwallowAllowedWithoutRectLimited:
+            default:
+                // 允许穿透，且不限定落点在节点内部
+                this._setClicked();
+                break;
+        }
+    }
+
+    private _setClicked() {
+        Singletons.log.i(`${this.node.name} Clicked`);
+        this._setColor(Color.WHITE);
+        // this.node.dispatchEvent(new MyEvent("clicked", true, this.node.name));
+    }
+
+    private _onTouched(e: EventTouch) {
+        this._setColor(this._color);
+    }
+
+    private _setColor(c: Color) {
+        this.node.getComponent(Sprite).color = c;
+    }
+
+    private _onClicked(e: MyEvent) {
+        Singletons.log.i(
+            `${this.node.name} Clicked`,
+            `current: ${e.currentTarget.name}`,
+            `from: ${e.target.name}`
+        );
+    }
+}
